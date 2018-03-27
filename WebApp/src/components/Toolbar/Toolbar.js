dd72de8e4ee78f457de29ae0304894fa58bdb56f
ikim23 <mikulas.zaymus@gmail.com>
2018-03-27 14:51:31 +0100

    upper username/button

--- WebApp/src/components/Toolbar/Toolbar.js
+++ WebApp/src/components/Toolbar/Toolbar.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import HamburgerButton from '../HamburgerButton/HamburgerButton';
+import UserButton from '../UserButton/UserButton';
 import './Toolbar.scss';
 
 export default class Toolbar extends Component {
@@ -8,6 +9,7 @@ export default class Toolbar extends Component {
     return (
       <div className="toolbar">
         <HamburgerButton onClick={this.props.toggleSidebar} />
+        <UserButton />
       </div>
     );
   }
