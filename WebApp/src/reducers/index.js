c30099ca158f19cc306504ec65aa50ffd05806e5
ikim23 <mikulas.zaymus@gmail.com>
2018-03-27 14:08:53 +0100

    remove boilerplate project code

--- WebApp/src/reducers/index.js
+++ WebApp/src/reducers/index.js
@@ -1,10 +1,8 @@
 import { combineReducers } from 'redux';
-import fuelSavings from './fuelSavingsReducer';
 import blocks from './blockReducer';
 import { routerReducer } from 'react-router-redux';
 
 const rootReducer = combineReducers({
-  fuelSavings,
   blocks,
   routing: routerReducer
 });
