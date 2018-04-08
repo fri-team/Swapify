735a183a76df059450321ce92eb3c1fedf7311b6
Marek Tavac <marek.tavac@fri.uniza.sk>
2018-04-08 21:48:57 +0200

    Constant name changed to more logic name, #40

--- Tests/BackendTest/Mongo2GoFixture.cs
+++ Tests/BackendTest/Mongo2GoFixture.cs
@@ -14,14 +14,14 @@ namespace BackendTest
 
     public class Mongo2GoFixture : IDisposable
     {
-        private const bool RUN_FOR_DEBUG = false;
+        private const bool RUN_WITH_STANDARD_PORT = false;
 
         private readonly MongoDbRunner _runner;
         public  IMongoClient MongoClient { get; }
 
         public Mongo2GoFixture()
         {
-            _runner = RUN_FOR_DEBUG ? MongoDbRunner.StartForDebugging() : MongoDbRunner.Start();
+            _runner = RUN_WITH_STANDARD_PORT ? MongoDbRunner.StartForDebugging() : MongoDbRunner.Start();
             MongoClient = new MongoClient(_runner.ConnectionString);
 
         }
