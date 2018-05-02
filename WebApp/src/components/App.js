54581035ad6e558e6e0e21358b5a7fcd90d6208a
ikim23 <mikulas.zaymus@gmail.com>
2018-05-02 12:20:42 +0200

    add React Router to project #55

--- WebApp/src/components/App.js
+++ WebApp/src/components/App.js
@@ -1,31 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Toolbar from './Toolbar/Toolbar';
-import SidebarContainer from '../containers/SidebarContainer/SidebarContainer';
-import TimetableContainer from '../containers/TimetableContainer/TimetableContainer';
-import BlockDetailContainer from '../containers/BlockDetailContainer/BlockDetailContainer';
+import { Route, Switch } from 'react-router-dom';
+import HomePage from './HomePage/HomePage';
+import TimetablePage from './TimetablePage/TimetablePage';
+import NotFoundPage from './NotFoundPage/NotFoundPage';
 
 // This is a class-based component because the current version of hot reloading
 // won't hot reload a stateless component at the top-level.
 
 class App extends React.Component {
-  state = {
-    sidebarOpen: true,
-  }
-
-  toggleSidebar = () => {
-    this.setState({ sidebarOpen: !this.state.sidebarOpen });
-  }
-
   render() {
     return (
-      <div className="container">
-        <Toolbar toggleSidebar={this.toggleSidebar} />
-        <div className="sidebar-timetable">
-          <SidebarContainer open={this.state.sidebarOpen} />
-          <TimetableContainer />
-        </div>
-        <BlockDetailContainer />
+      <div>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/timetable" component={TimetablePage} />
+          <Route component={NotFoundPage} />
+        </Switch>
       </div>
     );
   }
---
+++ WebApp/src/components/HomePage/HomePage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div>home</div>
+  );
+};
+
+export default HomePage;
