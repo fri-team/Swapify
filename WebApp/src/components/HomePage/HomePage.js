09cce365e9115c373998d5d0c2d0387b41bb77c1
ikim23 <mikulas.zaymus@gmail.com>
2018-10-13 11:20:14 +0200

    material-ui example

--- WebApp/src/components/HomePage/HomePage.js
+++ WebApp/src/components/HomePage/HomePage.js
@@ -1,30 +1,28 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import Button from '../Button/Button';
+import Button from '@material-ui/core/Button';
 import './HomePage.scss';
 
-const HomePage = (props) => {
-  return (
-    <div className="container home">
-      <div className="home-wrapper">
-        <h1>Swapify</h1>
-        <h3>Spravuj svoj rozvrh na jednom mieste!</h3>
-        <Button onClick={() => {
-          props.history.push('/register');
-        }}>Registrovať sa</Button>
-        <Button onClick={() => {
-          props.history.push('/login');
-        }}>Prihlásiť sa</Button>
-      </div>
+const HomePage = ({ history }) => (
+  <div className="container home">
+    <div className="home-wrapper">
+      <h1>Swapify</h1>
+      <h3>Spravuj svoj rozvrh na jednom mieste!</h3>
+      <Button
+        color="primary"
+        variant="contained"
+        onClick={() => history.push('/register')}
+      >
+        Registrovať sa
+      </Button>
+      <Button
+        color="primary"
+        variant="contained"
+        onClick={() => history.push('/login')}
+      >
+        Prihlásiť sa
+      </Button>
     </div>
-  );
-};
-
-
-HomePage.propTypes = {
-  history: PropTypes.shape({
-    push: PropTypes.func,
-  }).isRequired,
-};
+  </div>
+);
 
 export default HomePage;
--- WebApp/src/index.js
+++ WebApp/src/index.js
@@ -1,12 +1,11 @@
 /* eslint-disable import/default */
-
 import React from 'react';
 import { render } from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
 import configureStore, { history } from './store/configureStore';
 import Root from './components/Root';
-import './styles/styles.scss'; // Yep, that's right. You can import SASS/CSS files too! Webpack will run the associated loader and plug this into the page.
-require('./favicon.ico'); // Tell webpack to load favicon.ico
+import './styles/styles.scss';
+require('./favicon.ico');
 const store = configureStore();
 
 render(
