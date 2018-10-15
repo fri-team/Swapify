bd26161e989e563586f7abb7ca509f31ce93da76
tsh <mdomdo0@gmail.com>
2018-10-15 11:32:37 +0200

    Styling form

--- WebApp/src/components/StudyGroup/StudyGroup.js
+++ WebApp/src/components/StudyGroup/StudyGroup.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Toolbar from '../Toolbar/Toolbar';
 import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import SaveIcon from '@material-ui/icons/Save';
 import './StudyGroup.scss';
 
 class StudyGroup extends React.Component {
@@ -8,25 +10,28 @@ class StudyGroup extends React.Component {
     sidebarOpen: true,
   }
 
-  toggleSidebar = () => {
-    this.setState({ sidebarOpen: !this.state.sidebarOpen });
-  }
-
   render() {
     return (
-
       <div className="container home">
         <Toolbar toggleSidebar={this.toggleSidebar} />
         <div className="StudyGroup-wrapper">
-          <input type="text" placeholder="Zadajte skupinu" />
-          <Button
-            color="primary"
-            variant="contained"
-          >
-            Odoslat
-          </Button>
+            <TextField
+                label="Zadajte skupinu"
+                placeholder="Cislo skupiny"
+                margin="normal"
+                multiline={true}
+                fullWidth={true}
+             />
+            <br />
+            <br />
+             <Button
+                color="primary"
+                variant="fab"
+                mini
+                className="fab">
+               <SaveIcon/>
+             </Button>
         </div>
-
       </div>
     );
   }
