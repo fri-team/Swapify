c676d3e30dc12afd42d6e5bc1ed2fd53afd13dce
tsh <mdomdo0@gmail.com>
2018-10-15 20:52:41 +0200

    Study group select option, validation input...

--- WebApp/src/components/StudyGroup/StudyGroup.js
+++ WebApp/src/components/StudyGroup/StudyGroup.js
@@ -1,36 +1,75 @@
 import React from 'react';
 import Toolbar from '../Toolbar/Toolbar';
 import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
-import SaveIcon from '@material-ui/icons/Save';
+import NavigationIcon from '@material-ui/icons/Navigation';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import Input from '@material-ui/core/Input';
 import './StudyGroup.scss';
 
 class StudyGroup extends React.Component {
   state = {
     sidebarOpen: true,
+    group: ''
+  }
+
+  Submit = () => {
+    this.props.history.push('/timetable');
+  }
+
+  handleSubmit = (evt) => {
+    this.setState({ group: evt.target.value });
+  }
+
+  canBeSubmitted = () => {
+    const group = this.state.group;
+    return (
+      group != ''
+    );
+
   }
 
   render() {
+    const valid = this.canBeSubmitted();
     return (
       <div className="container home">
-        <Toolbar toggleSidebar={this.toggleSidebar} />
+        <Toolbar />
         <div className="StudyGroup-wrapper">
-            <TextField
-                label="Zadajte skupinu"
-                placeholder="Cislo skupiny"
-                margin="normal"
-                multiline={true}
-                fullWidth={true}
-             />
-            <br />
-            <br />
-             <Button
-                color="primary"
-                variant="fab"
-                mini
-                className="fab">
-               <SaveIcon/>
-             </Button>
+          <FormControl
+            fullWidth={true}
+          >
+            <InputLabel htmlFor="group">Zadajte štud. skupinu</InputLabel>
+            <Select
+              value={this.state.group}
+              onChange={this.handleSubmit}
+              input={<Input name="group" id="group" />}
+              autoWidth
+            >
+              <MenuItem value="">
+                <em>Nevybratá štud. skupina</em>
+              </MenuItem>
+              <MenuItem value={'5ZZS11'}>5ZZS11</MenuItem>
+              <MenuItem value={'5ZZS12'}>5ZZS12</MenuItem>
+              <MenuItem value={'5ZZS13'}>5ZZS13</MenuItem>
+              <MenuItem value={'5ZZS21'}>5ZZS21</MenuItem>
+              <MenuItem value={'5ZZS22'}>5ZZS22</MenuItem>
+              <MenuItem value={'5ZZS23'}>5ZZS23</MenuItem>
+            </Select>
+          </FormControl>
+          <br />
+          <br />
+          <Button
+            color="primary"
+            variant="fab"
+            mini
+            className="fab"
+            onClick={this.Submit}
+            disabled={!valid}
+          >
+            <NavigationIcon />
+          </Button>
         </div>
       </div>
     );
