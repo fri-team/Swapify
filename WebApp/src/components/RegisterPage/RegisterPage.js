03a78a0aedeb4ef45848a38283736d59115c6150
pavol.zvarik <zvarko5@gmail.com>
2018-10-23 23:07:26 +0200

    login & register frontend unification #26

--- WebApp/src/components/RegisterPage/RegisterPage.js
+++ WebApp/src/components/RegisterPage/RegisterPage.js
@@ -1,6 +1,3 @@
-// TODO: remove this "eslint-disable" after you remove all console logs...
-/* eslint-disable no-console */
-
 import React, { Component } from 'react';
 import {flatten ,values} from 'lodash';
 import classNames from 'classnames';
@@ -12,92 +9,88 @@ import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import { ElevatedBox, MacBackground } from '../';
 
-export default class RegisterPage extends Component {
-  constructor(props) {
-    super(props);
-
-    this.validator = new FormValidator([
-      {
-        field: 'email',
-        method: 'isEmpty',
-        validWhen: false,
-        message: 'Email je povinný.'
+class RegisterPage extends Component {
+  static validator = new FormValidator([
+    {
+      field: 'email',
+      method: 'isEmpty',
+      validWhen: false,
+      message: 'Zadajte email'
+    },
+    {
+      field: 'email',
+      method: 'isEmail',
+      validWhen: true,
+      message: 'Emailová adresa nie je platná.'
+    },
+    {
+      field: 'name',
+      method: 'isEmpty',
+      validWhen: false,
+      message: 'Zadajte meno'
+    },
+    {
+      field: 'surname',
+      method: 'isEmpty',
+      validWhen: false,
+      message: 'Zadajte priezvisko'
+    },
+    {
+      field: 'password',
+      method: 'isEmpty',
+      validWhen: false,
+      message: 'Zadajte heslo'
+    },
+    {
+      field: 'password',
+      method: this.passwordLength,
+      validWhen: true,
+      message: 'Heslo musí obsahovať aspoň 8 znakov.'
+    },
+    {
+      field: 'password',
+      method: 'matches',
+      args: [/\d/],
+      validWhen: true,
+      message: 'Heslo musí obsahovať aspoň jednu číslicu.'
+    },
+    {
+      field: 'password',
+      method: 'matches',
+      args: [/.*[A-Z]/],
+      validWhen: true,
+      message: 'Heslo musí obsahovať aspoň jedno veľké písmeno.'
+    },
+    {
+      field: 'password',
+      method: 'matches',
+      args: [/.*[a-z]/],
+      validWhen: true,
+      message: 'Heslo musí obsahovať aspoň jedno malé písmeno.'
+    },
+    {
+      field: 'passwordAgain',
+      method: 'isEmpty',
+      validWhen: false,
+      message: 'Zadajte heslo znova'
       },
-      {
-        field: 'email',
-        method: 'isEmail',
-        validWhen: true,
-        message: 'Emailová adresa nie je platná.'
-      },
-      {
-        field: 'name',
-        method: 'isEmpty',
-        validWhen: false,
-        message: 'Meno je povinné.'
-      },
-      {
-        field: 'surname',
-        method: 'isEmpty',
-        validWhen: false,
-        message: 'Priezvisko je povinné.'
-      },
-      {
-        field: 'password',
-        method: 'isEmpty',
-        validWhen: false,
-        message: 'Heslo je povinné.'
-      },
-      {
-        field: 'password',
-        method: this.passwordLength,
-        validWhen: true,
-        message: 'Heslo musí obsahovať aspoň 8 znakov.'
-      },
-      {
-        field: 'password',
-        method: 'matches',
-        args: [/\d/],
-        validWhen: true,
-        message: 'Heslo musí obsahovať aspoň jednu číslicu.'
-      },
-      {
-        field: 'password',
-        method: 'matches',
-        args: [/.*[A-Z]/],
-        validWhen: true,
-        message: 'Heslo musí obsahovať aspoň jedno veľké písmeno.'
-      },
-      {
-        field: 'password',
-        method: 'matches',
-        args: [/.*[a-z]/],
-        validWhen: true,
-        message: 'Heslo musí obsahovať aspoň jedno malé písmeno.'
-      },
-      {
-        field: 'passwordAgain',
-        method: 'isEmpty',
-        validWhen: false,
-        message: 'Potvrdenie hesla je povinné.'
-       },
-      {
-        field: 'passwordAgain',
-        method: this.passwordMatch,
-        validWhen: true,
-        message: 'Heslá sa nezhodujú.'
-      }
-    ]);
-
-    this.state = {
-      name: '',
-      surname: '',
-      email: '',
-      password: '',
-      passwordAgain: '',
-      validation: this.validator.valid(),
-      submitted: false,
-      serverErrors: []
+    {
+      field: 'passwordAgain',
+      method: this.passwordMatch,
+      validWhen: true,
+      message: 'Heslá sa nezhodujú'
     }
+  ]);
+
+  state = {
+    name: '',
+    surname: '',
+    email: '',
+    password: '',
+    passwordAgain: '',
+    validation: this.validator.valid(),
+    submitted: false,
+    serverErrors: []
   }
 
   passwordMatch = (confirmation, state) => (state.password === confirmation)
@@ -146,83 +139,87 @@ export default class RegisterPage extends Component {
     const serverErrorsList = serverErrors.map((e) => <li key={e}>{e}</li>);
 
     return (
-        <MacBackground>
-          <ElevatedBox>
-            <h1>Registrácia</h1>
-            <div className="register-form">
+      <MacBackground>
+        <ElevatedBox>            
+          <div className="register-form">
+            <TextField
+              label="Meno"
+              required
+              name = "name"
+              error={!!validation.name.message}
+              helperText={validation.name.message}
+              value={this.state.name}
+              fullWidth
+              onChange={this.handleInputChange}
+            />
+
+            <div className="register-form-spacer">
               <TextField
-                label="Meno"
-                required
-                name = "name"
-                error={!!validation.name.message}
-                helperText={validation.name.message}
-                value={this.state.name}
-                onChange={this.handleInputChange}
+                  label="Priezvisko"
+                  required
+                  name = "surname"
+                  error={!!validation.surname.message}
+                  helperText={validation.surname.message}
+                  value={this.state.surname}
+                  fullWidth
+                  onChange={this.handleInputChange}
               />
-
-              <div className="login-form-spacer">
-                <TextField
-                    label="Priezvisko"
-                    required
-                    name = "surname"
-                    error={!!validation.surname.message}
-                    helperText={validation.surname.message}
-                    value={this.state.surname}
-                    onChange={this.handleInputChange}
-                />
-              </div>
-
-              <div className="login-form-spacer">
-                <TextField
-                    label="Email"
-                    required
-                    name = "email"
-                    error={!!validation.email.message}
-                    helperText={validation.email.message}
-                    value={this.state.email}
-                    onChange={this.handleInputChange}
-                />
-              </div>
-
-              <div className="login-form-spacer">
-                <TextField
-                    label="Heslo"
-                    type="password"
-                    required
-                    name = "password"
-                    error={!!validation.password.message}
-                    helperText={validation.password.message}
-                    value={this.state.password}
-                    onChange={this.handleInputChange}
-                />
-              </div>
-
-              <div className="login-form-spacer">
-                <TextField
-                    label="Potvrdenie hesla"
-                    type="password"
-                    required
-                    name = "passwordAgain"
-                    error={!!validation.passwordAgain.message}
-                    helperText={validation.passwordAgain.message}
-                    value={this.state.passwordAgain}
-                    onChange={this.handleInputChange}
-                />
-              </div>
-
-              <Button
-                color="primary"
-                variant="contained"
-                onClick={this.onSubmit}
-              >
-                Registrovať
-              </Button>
-              <div className={classNames({ 'server-error': this.state.submitted && this.state.serverErrors.length > 0 })}>
-                <ul>{ serverErrorsList }</ul>
-              </div>
             </div>
-          </ElevatedBox>
-        </MacBackground>
+
+            <div className="register-form-spacer">
+              <TextField
+                  label="Email"
+                  required
+                  name = "email"
+                  error={!!validation.email.message}
+                  helperText={validation.email.message}
+                  value={this.state.email}
+                  fullWidth
+                  onChange={this.handleInputChange}
+              />
+            </div>
+
+            <div className="register-form-spacer">
+              <TextField
+                  label="Heslo"
+                  type="password"
+                  required
+                  name = "password"
+                  error={!!validation.password.message}
+                  helperText={validation.password.message}
+                  value={this.state.password}
+                  fullWidth
+                  onChange={this.handleInputChange}
+              />
+            </div>
+
+            <div className="register-form-spacer">
+              <TextField
+                  label="Potvrdenie hesla"
+                  type="password"
+                  required
+                  name = "passwordAgain"
+                  error={!!validation.passwordAgain.message}
+                  helperText={validation.passwordAgain.message}
+                  value={this.state.passwordAgain}
+                  fullWidth
+                  onChange={this.handleInputChange}
+              />
+            </div>
+
+            <Button
+              color="primary"
+              variant="contained"
+              onClick={this.onSubmit}
+            >
+              Registrovať
+            </Button>
+            <div className={classNames({ 'server-error': this.state.submitted && this.state.serverErrors.length > 0 })}>
+              <ul>{ serverErrorsList }</ul>
+            </div>
+          </div>
+        </ElevatedBox>
+      </MacBackground>
     );
   }
 }
@@ -232,3 +229,5 @@ RegisterPage.propTypes = {
     push: PropTypes.func,
   }).isRequired,
 }
+
+export default RegisterPage;
