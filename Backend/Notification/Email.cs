40aa3c9a193bc3a26ae12735e32a28d3a92b4a6a
davidm1766 <madaras.david1@gmail.com>
2018-10-29 15:21:16 +0100

    email fix #82

--- Backend/Notification/Email.cs
+++ Backend/Notification/Email.cs
@@ -7,7 +7,7 @@ namespace FRITeam.Swapify.Backend.Notification
     public class Email
     {
         public string FromEmail { get; set; } = "Mailgun Sandbox <postmaster @sandbox260b1e74a61b4b7faf43e42dd656e3a0.mailgun.org>";
-        public string ToEmail { get; set; } = "Dávid<madaras.david1@gmail.com>";
+        public string ToEmail { get; set; } = "ExampleName<youremail@gmail.com>";
         public string Body { get; set; }
         public string Subject { get; set; }
 
