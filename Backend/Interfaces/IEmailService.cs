3bb0378bc5a7cbe6a77e3c2d078951f8591477e7
Martin Dobiaš <dobias5@stud.uniza.sk>
2018-11-21 20:42:23 +0100

    #31-merge

--- Backend/Interfaces/IEmailService.cs
+++ Backend/Interfaces/IEmailService.cs
@@ -6,10 +6,6 @@ namespace FRITeam.Swapify.Backend.Interfaces
 {
     public interface IEmailService
     {
-        //void SendReqistrationMail(EmailBase data, string confirmationLink);
-        //void SendRestorePaswordMail(EmailBase data, string resetPasswordLink);
         void ComposeAndSendMail(EmailTypes typeOfEmail, EmailBase email, string generateLink);
-        //void SendMailMessage(MailMessage message);
-        //void ComposeMessage(EmailBase data);
     }
 }
