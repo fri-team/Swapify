a3ac0162e9242ea782ed901a1f1a06ccdac3ba7b
marek <pistekm@azet.sk>
2019-05-06 10:23:49 +0200

    todo notes

--- APIWrapper/Objects/ScheduleHourContent.cs
+++ APIWrapper/Objects/ScheduleHourContent.cs
@@ -69,12 +69,13 @@ namespace FRITeam.Swapify.APIWrapper.Objects
             StudyGroups.UnionWith(studyGroups);
         }
 
+        // TODO blocks with same time, course and place are also same
         public bool IsSameBlockAs(ScheduleHourContent b2)
         {
             return (CourseName == b2?.CourseName) &&
                     (TeacherName == b2?.TeacherName) &&
                     (RoomName == b2?.RoomName) &&
-                    (LessonType == b2?.LessonType) &&
+                    (LessonType == b2?.LessonType) &&                    
                     (StudyGroups.SetEquals(b2?.StudyGroups));
         }
 
