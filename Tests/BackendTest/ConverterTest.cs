806b9340f7a2ad87170b57f09b1db80122be8ae7
marek <pistekm@azet.sk>
2019-06-15 12:11:58 +0200

    write tests

--- Tests/BackendTest/ConverterTest.cs
+++ Tests/BackendTest/ConverterTest.cs
@@ -14,6 +14,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using FluentAssertions.Common;
+using FRITeam.Swapify.Backend.Interfaces;
 using Xunit;
 
 namespace BackendTest
@@ -56,7 +57,116 @@ namespace BackendTest
             var result = await serviceMock.Object.GetStudyGroupAsync("5ZZS99");
 
             Assert.Null(result);
-        }        
+        }
+
+        [Fact]
+        public async void ConvertTest_ConvertAndMergeSameConsecutiveBlocks()
+        {
+
+            // define schedule hour content for 4 blocks
+            var block1Hour1 =
+                new ScheduleHourContent(0, 1, false,
+                    LessonType.Laboratory, "teacher1",
+                    "room200", "sub1", "subject1", SubjectType.None);
+
+            var block1Hour2 =
+                new ScheduleHourContent(0, 2, false,
+                    LessonType.Laboratory, "teacher1",
+                    "room200", "sub1", "subject1", SubjectType.None);
+
+            // block with different course ant time
+            var block2Hour1 =
+                new ScheduleHourContent(0, 3, false,
+                    LessonType.Laboratory, "teacher1",
+                    "room200", "sub2", "subject2", SubjectType.None);
+
+            var block2Hour2 =
+                new ScheduleHourContent(0, 4, false,
+                    LessonType.Laboratory, "teacher1",
+                    "room200", "sub2", "subject2", SubjectType.None);
+
+            // same time as block 2
+            var block3Hour1 =
+                new ScheduleHourContent(0, 3, false,
+                    LessonType.Laboratory, "teacher2",
+                    "room201", "sub2", "subject2", SubjectType.None);
+
+            var block3Hour2 =
+                new ScheduleHourContent(0, 4, false,
+                    LessonType.Laboratory, "teacher2",
+                    "room201", "sub2", "subject2", SubjectType.None);
+
+            var block4Hour1 =
+                new ScheduleHourContent(4, 5, false,
+                    LessonType.Laboratory, "teacher2",
+                    "room201", "sub2", "subject2", SubjectType.None);
+
+            var block4Hour2 =
+                new ScheduleHourContent(4, 6, false,
+                    LessonType.Laboratory, "teacher2",
+                    "room201", "sub2", "subject2", SubjectType.None);
+
+
+            var scheduleHourContents = new List<ScheduleHourContent>()
+            {
+                block3Hour1, block1Hour1, block4Hour2, block2Hour2, block1Hour2, block3Hour2, block4Hour1, block2Hour1
+            };
+
+            // create course service mock
+            var guid = Guid.NewGuid();
+            var courseServiceMock = new Mock<ICourseService>();
+            courseServiceMock.Setup(m => m.GetOrAddNotExistsCourseId(It.IsAny<string>(), It.IsAny<Block>())).ReturnsAsync(guid);
+
+            // execute
+            var timetable = await ConverterApiToDomain.ConvertAndMergeSameConsecutiveBlocks(scheduleHourContents, courseServiceMock.Object, false);
+
+            // define result blocks
+            var expectedResult = new List<Block>()
+            {
+                new Block()
+                {
+                    Day = Day.Monday,
+                    StartHour = 7,
+                    Duration = 2,
+                    Room = "room200",
+                    Teacher = "teacher1",
+                    BlockType = BlockType.Laboratory,
+                    CourseId = guid
+                },
+                new Block()
+                {
+                    Day = Day.Monday,
+                    StartHour = 9,
+                    Duration = 2,
+                    Room = "room200",
+                    Teacher = "teacher1",
+                    BlockType = BlockType.Laboratory,
+                    CourseId = guid
+                },
+                new Block()
+                {
+                    Day = Day.Monday,
+                    StartHour = 9,
+                    Duration = 2,
+                    Room = "room201",
+                    Teacher = "teacher2",
+                    BlockType = BlockType.Laboratory,
+                    CourseId = guid                   
+                },
+                new Block()
+                {
+                    Day = Day.Friday,
+                    StartHour = 11,
+                    Duration = 2,
+                    Room = "room201",
+                    Teacher = "teacher2",
+                    BlockType = BlockType.Laboratory,
+                    CourseId = guid
+                },                
+            };
+            
+            timetable.AllBlocks.Should().BeEquivalentTo(expectedResult);
+        }
 
         [Fact]
         public void ConvertTest_MergeSameBlocksWithDifferentTeacher()
