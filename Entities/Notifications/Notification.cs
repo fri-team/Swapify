bc23a45ed3d9b05ec285bbf9f0df7ab4234aecd8
marek <pistekm@azet.sk>
2019-06-23 21:49:11 +0200

    #176 notification type as string

--- Entities/Notifications/Notification.cs
+++ Entities/Notifications/Notification.cs
@@ -1,6 +1,7 @@
 using System;
 using FRITeam.Swapify.Entities.Enums;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
 
 namespace FRITeam.Swapify.Entities.Notifications
 {
@@ -9,6 +10,7 @@ namespace FRITeam.Swapify.Entities.Notifications
         [JsonIgnore]
         public Guid RecipientId { get; set; }
 
+        [JsonConverter(typeof(StringEnumConverter))]
         public NotificationType Type { get; set; }
         
         public abstract string Text { get; }        
