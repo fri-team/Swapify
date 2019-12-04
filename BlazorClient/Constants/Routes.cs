53efd0440f53cd15bd21e6b2e7ed228393fa463b
marek <pistekm@azet.sk>
2019-12-04 14:17:53 +0100

    redirect after successful registration, login, password reset, new password set

---
+++ BlazorClient/Constants/Routes.cs
@@ -0,0 +1,12 @@
+namespace BlazorClient.Constants
+{
+    public static class Routes
+    {
+        public static string HOME = "/";
+        public static string LOGIN = "/";
+        public static string RESET_PASSWORD = "/resetpassword";
+        public static string REGISTER = "/register";
+        public static string SET_NEW_PASSWORD = "/set-new-password/";
+        public static string TIMETABLE = "/timetable";
+    }
+}
