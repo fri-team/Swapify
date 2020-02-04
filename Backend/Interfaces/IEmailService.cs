5ef8d212f750d666622e7c2706fbf989fd1637de
MatejKopera <matej.kopera@gmail.com>
2020-02-04 15:46:09 +0100

    -now using EmailBase class -user and password defined in appsettings.json -renamed from "MailUs" to "SendFeedback" for more accurate description -extended appsetting MailingSettings with new attribute FeedbackEmail where feedback will be sent

--- Backend/Interfaces/IEmailService.cs
+++ Backend/Interfaces/IEmailService.cs
@@ -3,6 +3,7 @@ namespace FRITeam.Swapify.Backend.Interfaces
 {
     public interface IEmailService
     {
+        bool SendFeedbackEmail(string receiver, string context);
         bool SendConfirmationEmail(string receiver, string confirmationLink, string emailType);
     }
 }
