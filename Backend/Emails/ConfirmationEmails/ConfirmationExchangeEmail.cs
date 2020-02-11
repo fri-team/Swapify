8ee11219949282e75635d6eacff4b2775903f761
Martin Dobiaš <dobias5@stud.uniza.sk>
2020-02-11 22:55:32 +0100

    288_Edit_template_path

--- Backend/Emails/ConfirmationEmails/ConfirmationExchangeEmail.cs
+++ Backend/Emails/ConfirmationEmails/ConfirmationExchangeEmail.cs
@@ -5,7 +5,7 @@ namespace FRITeam.Swapify.Backend.Emails
     class ConfirmExchangeEmail : ConfirmationEmailBase
     {
         protected override string Subject => "Swapify výmena";
-        protected override string PathToTemplate => @"Emails/EmailTemplates/ConfirmEmailTemplate.html";
+        protected override string PathToTemplate => @"Emails/EmailTemplates/ConfirmationExchangeTemplate.html";
 
         public ConfirmExchangeEmail(ILoggerFactory loggerFactory, string sender, string senderDisplayName, string receiver, string baseUrl, string confirmationLink)
             : base(loggerFactory, sender, senderDisplayName, receiver, baseUrl, confirmationLink)
