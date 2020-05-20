eb1c67407f3b1803d31cbf094e3f17ec44246222
branislavtvrdy <branislav.tvrdy@gmail.com>
2020-05-20 15:53:41 +0200

    Fix for adding new block.

--- Backend/CourseService.cs
+++ Backend/CourseService.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using FRITeam.Swapify.Backend.Interfaces;
 using FRITeam.Swapify.Entities;
@@ -50,8 +51,13 @@ namespace FRITeam.Swapify.Backend
 
         public List<Course> FindByStartName(string courseStartsWith)
         {
-            var filter = "{CourseName: /" + courseStartsWith + "/ }"; //regex for search on every position in course name 
-            return _courseCollection.Find(filter).ToList();
+            var filter = "{CourseName: /" + courseStartsWith + "/ }"; //regex for search on every position in course name
+            return _courseCollection.Find(x => x.CourseName.ToLower().Contains(courseStartsWith.ToLower())).ToList();
+            // Might want to order selection
+//            var temp = _courseCollection.Find(x => x.CourseName.ToLower().Contains(courseStartsWith.ToLower())).ToList();
+//            temp.OrderBy(x => x.CourseName.IndexOf(courseStartsWith)).ThenBy(x =>x.CourseName).ToList();
+//            return temp;
+//            return _courseCollection.Find(filter).ToList();
         }
 
         /// <summary>
