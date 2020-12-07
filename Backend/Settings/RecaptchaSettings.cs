49ec27177baf82acb57f1463b38b553c0267013d
CholujMartin <martincholuj284@gmail.com>
2020-12-07 17:53:38 +0100

    Additinal commit

---
+++ Backend/Settings/RecaptchaSettings.cs
@@ -0,0 +1,8 @@
+namespace FRITeam.Swapify.Backend.Settings
+{
+    public class RecaptchaSettings : SettingsBase
+    {
+        public string PrivateKey { get; set; }
+        public string URL { get; set; }
+    }
+}
