19d3c7900a1a18a96fbe485f299498c636ecd767
BCIT\branislav <brano1414@gmail.com>
2020-12-09 14:57:52 +0100

    course_comment

--- Entities/Course.cs
+++ Entities/Course.cs
@@ -5,6 +5,10 @@ namespace FRITeam.Swapify.Entities
 {
     public class Course : BaseEntity
     {
+        /// <summary>
+        /// Course code in specific format
+        /// e.g. 5II210
+        /// </summary>
         public string CourseCode { get; set; }        
         public string CourseName { get; set; }
         public Timetable Timetable { get; set; }
