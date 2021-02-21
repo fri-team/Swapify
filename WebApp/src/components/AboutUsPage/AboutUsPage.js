2fa80eeca5685f59e52a33ab5bd4d780f6896766
Marek Zorvan <marek.zorvan@siemens.com>
2021-02-21 16:17:00 +0100

    fixes

--- WebApp/src/components/AboutUsPage/AboutUsPage.js
+++ WebApp/src/components/AboutUsPage/AboutUsPage.js
@@ -4,7 +4,10 @@ import GlLogo from '../../images/GlobalLogic-logo-black.png';
 import UnizaLogo from '../../images/Uniza-logo-black.png';
 
 class AboutUsPage extends Component {
-  
+  constructor() {
+    super();
+  }
+
   render() {
     return (
       <div className="AboutUsContainer">
@@ -15,12 +18,12 @@ class AboutUsPage extends Component {
           ale budú si môcť predmety vymeniť jednoducho cez aplikáciu. Malo by to byť miestom, kde si môžeš predmet zmeniť a systém za Teba nájde osobu, ktorá chce opačnú výmenu.
         </p>
         <p>
-          We are students from Faculty of informatics and management science. We are trying to create an <strong>unique app</strong> for students of University of Žilina (firstly just our faculty), 
-          where they can change their timetables and each subjects. The whole idea is that students don´t have to seek for someone who want to <strong>change their subject in timetable</strong> too, 
+          We are students from Faculty of informatics and management science. We are trying to create an <strong>unique app</strong> for students of University of Žilina (firstly just our faculty),
+          where they can change their timetables and each subjects. The whole idea is that students don´t have to seek for someone who want to <strong>change their subject in timetable</strong> too,
           but they can easily change it via the app. It should be the place where you can make changes, and system will find you another person who wants reverse change.
         </p>
         <div className="AboutUsLogos">
-          <img src={GlLogo} alt="Global Logic logo" width="807" height="151"/>
+          <img src={GlLogo} alt="Global Logic logo" width="807" height="151" />
           <img src={UnizaLogo} alt="Uniza logo" width="4082" height="4096" />
         </div>
       </div>
