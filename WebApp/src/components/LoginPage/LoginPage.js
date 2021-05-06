997d5c7be176f3364cf709f44a80853f09f4cc00
BCIT\branislav <brano1414@gmail.com>
2021-05-06 10:34:33 +0200

    317_test

--- WebApp/src/components/LoginPage/LoginPage.js
+++ WebApp/src/components/LoginPage/LoginPage.js
@@ -189,7 +189,7 @@ class LoginPage extends PureComponent {
 
             <div className="FormField captchaClass">
               <ReCAPTCHA
-                sitekey="6LeJhgIaAAAAAAyNiupTgRYPQGEOCQc7WvvzR8ue"
+                sitekey="##RECAPTCHA_FRONT_END_KEY##"
                 onChange={this.onChangeCaptcha}
                 hl="sk"
               />
