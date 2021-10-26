547c18df98e0defc1de8bb47740fc92d0a483ec9
martin <martincholuj284@gmail.com>
2021-10-26 17:24:10 +0200

    Fixed blank page after first login and personal number confirmation

--- WebAPI/Controllers/StudentController.cs
+++ WebAPI/Controllers/StudentController.cs
@@ -56,7 +56,8 @@ namespace WebAPI.Controllers
 
             if (student.Timetable == null)
             {
-                return ErrorResponse($"Timetable for student with id: {studentId} does not exist.");
+                _logger.Log(LogLevel.Error, $"Timetable for student with id: {studentId} does not exist.");
+                return Ok(new Timetable());
             }
 
             var timetable = new Timetable();
