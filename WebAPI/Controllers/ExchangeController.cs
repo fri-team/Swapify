57f6985c7cccc8556c475b62039e151788e9c17a
Radovan Filický <rado.filickyp@gmail.com>
2021-11-28 09:25:27 +0100

    Cleaned commented code.
    I jsut removed commented code from last commit.

--- WebAPI/Controllers/ExchangeController.cs
+++ WebAPI/Controllers/ExchangeController.cs
@@ -83,16 +83,13 @@ namespace WebAPI.Controllers
                     {
                         string message = $"Error when sending confirmation email to user {user1.Email}.";
                         _logger.LogError(message);
-                        //return NotFound(message);
                     }
 
                     if (!_emailService.SendConfirmationEmail(user2.Email, callbackUrl2, "ConfirmExchangeEmail"))
                     {
                         string message = $"Error when sending confirmation email to user {user2.Email}.";
                         _logger.LogError(message);
-                        //return NotFound(message);
                     }
-
                     return Ok(res);
                 }
                 return Ok(null);
