af3488fbbe4726bce51b78e90c41dc9db344a0d3
flacore <m.molitoris@yahoo.sk>
2022-04-09 15:21:16 +0200

    LDAP loggin passed

--- Backend/AuthenticatorLdap.cs
+++ Backend/AuthenticatorLdap.cs
@@ -17,13 +17,13 @@ namespace FRITeam.Swapify.Backend
             UserInformations userInformations = null;
             logger.LogDebug("LDAP: Trying connection to LDAP");
             var connection = new LdapConnection { SecureSocketLayer = _options.SecureSocketLayer };
-            string[] attributes = new[] { "samaccountname", "displayname", "uidnumber", "mail" };
+            string[] attributes = new[] { "samaccountname", "displayname", "employeeNumber", "mail" };
             connection.Connect(_options.HostName, _options.Port);
             connection.Bind(username, password);
             if (connection.Bound)
             {
                 logger.LogDebug("LDAP: Connected to LDAP with username " + username);
-                var results = connection.Search(_options.BaseDN, LdapConnection.ScopeSub,
+                ILdapSearchResults results = connection.Search(_options.BaseDN, LdapConnection.ScopeSub,
                     $"samaccountname={username.Split("@")[0]}", attributes, false);
 
                 if (results.HasMore())
@@ -34,7 +34,7 @@ namespace FRITeam.Swapify.Backend
                     {
                         Name = attributeSet.GetAttribute("displayname")?.StringValue,
                         Login = attributeSet.GetAttribute("samaccountname")?.StringValue,
-                        PersonalNumber = attributeSet.GetAttribute("uidnumber")?.StringValue,
+                        PersonalNumber = attributeSet.GetAttribute("employeeNumber")?.StringValue,
                         Email = attributeSet.GetAttribute("mail")?.StringValue,
                     };
                 }
