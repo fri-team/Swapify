d1f5589e10fdee150bd24d8e882121664f9c86aa
flacore <m.molitoris@yahoo.sk>
2022-05-05 00:13:58 +0200

    HTML - todo css

--- WebApp/src/components/Announcement/AnnouncementBlock.js
+++ WebApp/src/components/Announcement/AnnouncementBlock.js
@@ -4,9 +4,9 @@ import '../../styles/styles.scss';
 
 export default class AnnouncementBlock extends PureComponent {
 
-    getTextOfAnnouncement = () =>{
+    getTextOfAnnouncement = () =>{ 
         //Sem zadaj text oznamu
-        var text = [
+        const text = [
             "Od dňa 02.05.2022 sme úspešne implementovaly nový spôsob prihlasovania. Od tohto dňa využívame celouniverzitné prihlasovanie "
             +"do systému vďaka čomu je možné využívať aplikáciu všetkými študentmi Univerzity v Žiline.",
             "Pre užívateľov, ktorí už boli registrované netreba nič meniť, iba sa prihlásiť s použitím celouniverzitného hesla.",
@@ -15,9 +15,36 @@ export default class AnnouncementBlock extends PureComponent {
         return text;
     }
 
-    doesAnnouncementTextExist = () =>{
-        //Ak text nieje zadany nastav false, inak nastav true
-        return true; 
+    doesAnnouncementTextExist = () => {
+        var text = this. getTextOfAnnouncement();
+        if(text != null)
+            return true;
+        else
+            return false; 
+    }
+
+    getFormatedText = () => {
+        var text = this. getTextOfAnnouncement();
+        if(text.isArray()){
+            var components = "";
+
+            for(var i=0; i < text.size(); i++){
+                components += this.textToElement(text[i]);
+            }
+
+            return components;
+
+        }else{
+            return this.textToElement(text);
+        }
+    }
+
+    textToElement = (text) => {
+        return (
+            <p class="AnnouncementText">
+                {text}
+            </p>
+        );
     }
 
     render() {    
@@ -25,7 +52,7 @@ export default class AnnouncementBlock extends PureComponent {
             return (
                 <div class="AnnouncementDiv">
                     <div class="InsideDiv">
-                        
+                        {this.getFormatedText()}
                     </div>
                 </div>
             );
