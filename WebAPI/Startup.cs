5c28fa12cc799a028da0de036872e7abed32b048
Radovan Filický <rado.filickyp@gmail.com>
2022-11-13 12:08:52 +0100

    fixed code
    this part of code somehow got corrupted

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -71,9 +71,10 @@ namespace WebAPI
             }
             else
             {
-                try
+                services.Configure<SwapifyDatabaseSettings>(Configuration.GetSection(nameof(SwapifyDatabaseSettings)));
+                var settings = new MongoClientSettings
                 {
-                    Server = new MongoServerAddress("mongodb-stg", 27017),
+                    Server = new MongoServerAddress("mongodb", 27017),
                     GuidRepresentation = GuidRepresentation.Standard
                 };
                 services.AddSingleton(new MongoClient(settings).GetDatabase(_swapifyDbSettings.DatabaseName));
