22602853345691b778614ebaeea12484d28e2dde
Simona71830 <skombarova.simona@gmail.com>
2023-02-04 19:53:34 +0100

    menu on toolbar needs function to close

--- WebApp/src/components/Toolbar/Toolbar.js
+++ WebApp/src/components/Toolbar/Toolbar.js
@@ -1,27 +1,27 @@
-import React, { PureComponent } from 'react';
-import styled from 'styled-components';
-import { connect } from 'react-redux';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
-import HelpIcon from '@material-ui/icons/Help';
-import UserAvatar from './UserAvatar';
-import Menu from './Menu';
-import { PullRight } from './shared';
-import { Button } from '@material-ui/core';
-import { bindActionCreators } from 'redux';
-import * as userActions from '../../actions/userActions';
-import * as timetableActions from '../../actions/timetableActions';
-import { withRouter } from 'react-router-dom';
-import { PERSONALNUMBER, TIMETABLE } from '../../util/routes';
-import NotificationPanel from '../Notifications/NotificationPanel';
-import Tooltip from '@material-ui/core/Tooltip';
-import Zoom from '@material-ui/core/Zoom';
-import './Toolbar.scss';
-import MailIcon from '@material-ui/icons/Mail';
-import logo from '../../images/logowhite.png';
-import CalendarIcon from '@material-ui/icons/CalendarToday'
+import React, { PureComponent } from "react";
+import styled from "styled-components";
+import { connect } from "react-redux";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import HelpIcon from "@material-ui/icons/Help";
+import UserAvatar from "./UserAvatar";
+import Menu from "./Menu";
+import { PullRight } from "./shared";
+import { Button } from "@material-ui/core";
+import { bindActionCreators } from "redux";
+import * as userActions from "../../actions/userActions";
+import * as timetableActions from "../../actions/timetableActions";
+import { withRouter } from "react-router-dom";
+import { PERSONALNUMBER, TIMETABLE } from "../../util/routes";
+import NotificationPanel from "../Notifications/NotificationPanel";
+import Tooltip from "@material-ui/core/Tooltip";
+import Zoom from "@material-ui/core/Zoom";
+import "./Toolbar.scss";
+import MailIcon from "@material-ui/icons/Mail";
+import logo from "../../images/logowhite.png";
+import CalendarIcon from "@material-ui/icons/CalendarToday";
 
 const ToolbarWrapper = styled.div`
   width: 100%;
@@ -43,15 +43,14 @@ class AppToolbar extends PureComponent {
   timetable = () => this.props.history.push(TIMETABLE);
 
   checkUrl = () => {
-    if(window.location.pathname == "/personal-number") {
+    if (window.location.pathname == "/personal-number") {
       return true;
     } else {
       return false;
     }
-  }
+  };
 
   render() {
-
     let buttonExchangeMode;
     if (this.props.timetable.isExchangeMode) {
       buttonExchangeMode = (
@@ -72,7 +71,7 @@ class AppToolbar extends PureComponent {
 
     let buttonAddMode;
     if (
-      this.props.timetable.displayedCourses.length > 0  &&
+      this.props.timetable.displayedCourses.length > 0 &&
       !this.props.timetable.isExchangeMode
     ) {
       buttonAddMode = (
@@ -90,30 +89,44 @@ class AppToolbar extends PureComponent {
       );
     }
 
-    const { user, toggleSidebar, exportCalendar, toggleHelpModalWindow, toggleMailUsModalWindow, changeDarkMode } = this.props;
+    const {
+      user,
+      toggleSidebar,
+      exportCalendar,
+      toggleHelpModalWindow,
+      toggleMailUsModalWindow,
+      changeDarkMode,
+    } = this.props;
     const url = this.checkUrl();
     return (
       <ToolbarWrapper>
-        <AppBar position="static" color={ this.props.darkMode ? "secondary" : "primary" }>
+        <AppBar
+          position="static"
+          color={this.props.darkMode ? "secondary" : "primary"}
+        >
           <Toolbar>
-            { !url && (
-            <IconButton
-              color="inherit"
-              aria-label="Menu"
-              onClick={toggleSidebar}
-            >
-              <MenuIcon />
-            </IconButton>
+            {!url && (
+              <IconButton
+                color="inherit"
+                aria-label="Menu"
+                onClick={toggleSidebar}
+              >
+                <MenuIcon />
+              </IconButton>
             )}
-
             <IconTray>
-
-            <img src={logo} alt="logo" height="30px" className="logowhite" onClick={this.timetable}/>
+              <img
+                src={logo}
+                alt="logo"
+                height="30px"
+                className="logowhite"
+                onClick={this.timetable}
+              />
               <PullRight />
               {buttonExchangeMode}
               {buttonAddMode}
               <UserAvatar
-                ref={ref => (this.anchor = ref)}
+                ref={(ref) => (this.anchor = ref)}
                 username={user.name}
                 onClick={() => this.setState({ showMenu: true })}
               />
@@ -125,16 +138,24 @@ class AppToolbar extends PureComponent {
                   email={user.email}
                   selectPersonalNumber={this.changePersonalNumber}
                   onLogout={this.handleLogout}
-                  onClose={() => this.setState({ showMenu: false })}
+                  //onClose={() => this.setState({ showMenu: false })}
+                  //onClick={() => this.setState({ showMenu: !this.state.showMenu })}
                   changeDarkMode={changeDarkMode}
                 />
               )}
             </IconTray>
             &nbsp;
-            <p onClick={() => this.setState({ showMenu: true })} className="cursor">
+            <p
+              onClick={() => this.setState({ showMenu: true })}
+              className="cursor"
+            >
               {user.name} {user.surname}
             </p>
-            <Tooltip title="Stiahnuť kalendár" placement="top" TransitionComponent={Zoom}>
+            <Tooltip
+              title="Stiahnuť kalendár"
+              placement="top"
+              TransitionComponent={Zoom}
+            >
               <IconButton
                 color="inherit"
                 aria-label="CalendarExport"
@@ -143,7 +164,11 @@ class AppToolbar extends PureComponent {
                 <CalendarIcon />
               </IconButton>
             </Tooltip>
-            <Tooltip title="Zobraz pomocník" placement="top" TransitionComponent={Zoom}>
+            <Tooltip
+              title="Zobraz pomocník"
+              placement="top"
+              TransitionComponent={Zoom}
+            >
               <IconButton
                 color="inherit"
                 aria-label="Help"
@@ -152,8 +177,11 @@ class AppToolbar extends PureComponent {
                 <HelpIcon />
               </IconButton>
             </Tooltip>
-
-            <Tooltip title="Napíšte nám" placement="top" TransitionComponent={Zoom}>
+            <Tooltip
+              title="Napíšte nám"
+              placement="top"
+              TransitionComponent={Zoom}
+            >
               <IconButton
                 color="inherit"
                 aria-label="MailUs"
@@ -162,13 +190,13 @@ class AppToolbar extends PureComponent {
                 <MailIcon />
               </IconButton>
             </Tooltip>
-
-            <Tooltip title="Notifikácie" placement="top" TransitionComponent={Zoom}>
-              <IconButton
-                color="inherit"
-                aria-label="Notifications"
-              >
-                <NotificationPanel darkMode={this.props.darkMode}/>
+            <Tooltip
+              title="Notifikácie"
+              placement="top"
+              TransitionComponent={Zoom}
+            >
+              <IconButton color="inherit" aria-label="Notifications">
+                <NotificationPanel darkMode={this.props.darkMode} />
               </IconButton>
             </Tooltip>
           </Toolbar>
@@ -178,14 +206,14 @@ class AppToolbar extends PureComponent {
   }
 }
 
-const mapStateToProps = state => ({
+const mapStateToProps = (state) => ({
   user: state.user,
-  timetable: state.timetable
+  timetable: state.timetable,
 });
 
-const mapDispatchToProps = dispatch => ({
+const mapDispatchToProps = (dispatch) => ({
   userActions: bindActionCreators(userActions, dispatch),
-  timetableActions: bindActionCreators(timetableActions, dispatch)
+  timetableActions: bindActionCreators(timetableActions, dispatch),
 });
 
 export default connect(
